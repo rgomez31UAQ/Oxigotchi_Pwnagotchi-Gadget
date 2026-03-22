c90e5a9a9040b3be96902a7fb71d209dae7356c8
PwnPacker <noreply@github.com>
2026-03-22 23:45:14 +0200

    refactor: simplify ip_display plugin, rotation now handled by Rust

--- rust/plugins/ip_display.lua
+++ rust/plugins/ip_display.lua
@@ -1,12 +1,10 @@
 -- ip_display.lua: IP address display.
--- RAGE mode: USB IP only. SAFE mode: rotates USB/BT.
+-- Rust controls rotation: RAGE = USB only, SAFE = rotates USB/BT every 5s.
 plugin = {}
 plugin.name    = "ip_display"
-plugin.version = "2.0.0"
+plugin.version = "3.0.0"
 plugin.author  = "oxigotchi"
 plugin.tag     = "default"
-
-local show_bt = false
 
 function on_load(config)
     register_indicator("ip_display", {
@@ -17,16 +15,5 @@ function on_load(config)
 end
 
 function on_epoch(state)
-    if state.mode == "RAGE" then
-        -- RAGE: USB tether only (BT is off)
-        set_indicator("ip_display", state.display_ip)
-    else
-        -- SAFE: rotate between USB and BT IP
-        if show_bt and state.bt_connected and state.bt_ip ~= "" then
-            set_indicator("ip_display", "BT:" .. state.bt_ip)
-        else
-            set_indicator("ip_display", state.display_ip)
-        end
-        show_bt = not show_bt
-    end
+    set_indicator("ip_display", state.display_ip)
 end
